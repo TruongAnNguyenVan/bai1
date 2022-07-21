39ef287cc72bd7fcf868127e5880a82fad681c4b
TruongAnNguyenVan <109612341+TruongAnNguyenVan@users.noreply.github.com>
2022-07-21 22:25:14 +0700

    tìm diện tích tam giác

---
+++ HomeWork/bai3.cpp
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+	int a;
+	printf("Nhap a: ");
+	scanf("%d",&a);
+	
+	int b;
+	printf("Nhap b: ");
+	scanf("%d",&b);
+	
+	int c;
+	printf("Nhap c: ");
+	scanf("%d",&c);
+	
+	double p;
+	double d;
+	double s; 
+	
+	if(a + b > c){
+		p = (a+b+c)/2;
+		d = a + b + c;
+		s = sqrt(p*(p-a)*(p-b)*(p-c));
+		printf("a,b,c là ba canh cua mot tam giac co: \n");
+		printf("Chu vi la: %f \n",d);
+		printf("Dien tich la: %f",s); 
+	}else{
+		printf("a,b,c khong phai la ba canh cua mot tam giac"); 
+	} 
+} 
